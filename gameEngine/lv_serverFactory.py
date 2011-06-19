891ca227a8ba41066c0c54ca8359b5f3bdc2ed17
Florian R. A. Angermeier <florian.angermeier@desaster-games.com>
2011-06-19 23:57:19 +0200

    Removed unnecessary files.

--- gameEngine/lv_serverFactory.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/python
-#*** encoding:utf-8 ***
-
-from twisted.internet import protocol
-
-from lv_protocol import lv_protocol
-
-class lv_serverFactory( protocol.ServerFactory ):
-	protocol = lv_protocol
